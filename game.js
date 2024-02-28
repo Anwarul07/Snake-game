26081c4af28bd9d9805aec45d4e6f7d62eec298e
Anwarul07 <mdanwarul064@gmail.com>
2024-02-28 22:23:12 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,10 +1,11 @@
 //Constant And Varibles 
-const direction = { x: 0, y: 0 };
+const inputDir = { x: 0, y: 0 };
 const foodSound = new Audio("Files/food.mp3");
 const gameOver = new Audio("Files/gameover.mp3");
 const moveSound = new Audio("Files/move.mp3");
 const musicSound = new Audio("Files/music.mp3");
 const speed = 2;
+let scor = 0;
 let Section = document.querySelector(".section")
 let lastpaintTime = 0;
 let snakeArr = [{ x: 13, y: 15 }]
@@ -26,7 +27,26 @@ function main(currTime) {
 }
 
 // Display food and snake items 
+
+function isCollide(sArr) {
+    return false;
+    
+}
 function gameEngne() {
+
+    if (isCollide(snakeArr)) {
+        gameOver.play();
+        musicSound.paued();
+        inputDir = { x: 0, y: 0 };
+        alert("Game Over ! pls press any key to play again");
+        snakeArr = [{ x: 13, y: 15 }];
+        Score = 0;
+
+    }
+
+
+
+
     Section.innerHTML = '';
     snakeArr.forEach((e, index) => {
         //Displaying the location and Snake
@@ -36,7 +56,7 @@ function gameEngne() {
         snakeElement.style.gridColumnStart = e.x;
         if (index === 0) {
             snakeElement.classList.add("head");
-        } else{
+        } else {
             snakeElement.classList.add("snake");
         }
         Section.appendChild(snakeElement);
@@ -53,6 +73,38 @@ function gameEngne() {
 
 }
 
+//Main LOgic Starts
 window.requestAnimationFrame(main);
+window.addEventListener("keydown", e => {
+    inputDir = { x: 0, y: 1 }// Start the Game
+    moveSound.play();
+
+    switch (e.key) {
+        case "ArrowUp":
+            console.log("ArrowUp Pressed");
+            inputDir.x = 0;
+            inputDir.y = -1;
+            break;
+
+        case "ArrowDown":
+            console.log("ArrowDown Pressed");
+            inputDir.x = 0;
+            inputDir.y = 1;
+            break;
+        case "Arrowleft":
+            console.log("Arrowleft Pressed");
+            inputDir.x = -1;
+            inputDir.y = 0;
+            break;
+
+        case "ArrowRight":
+            console.log("ArrowRight Pressed");
+            inputDir.x = 1;
+            inputDir.y = 0;
+            break;
+        default:
+            break;
+    }
+})
 
 
